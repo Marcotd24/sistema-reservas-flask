19c27816fb26e864962f5b92ae5e448db0e7053e
Marcotd24 <marco2001pambi@gmail.com>
2026-02-12 22:38:12 -0500

    Primer avance proyecto Flask funcionando

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return "Bienvenido al Sistema de Reservas Médicas – Clínica SaludPlus"
+
+@app.route('/cita/<paciente>')
+def cita(paciente):
+    return f"Hola {paciente}, tu cita médica está en proceso de confirmación."
+
+if __name__ == '__main__':
+    app.run(debug=True)
